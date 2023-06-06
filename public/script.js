eec2d4cdaf29bee464b4be607919dfcbcc9f42ec
Aum Kulkarni <aumwinay@gmail.com>
2023-06-07 03:27:05 +0530

    Made some changes

--- public/script.js
+++ public/script.js
@@ -6,11 +6,12 @@ submitForm.addEventListener("submit", (e) => {
     let name = document.getElementsByName("name")[0].value;
     let email = document.getElementsByName("email")[0].value;
     let message = document.getElementsByName("message")[0].value;
-    console.log(message);
+    let subject = document.getElementsByName("subject")[0].value;
     document.getElementsByName("message")[0].value = '';
     let reqBody = {
         'name': name,
         'email': email,
+        'subject': subject,
         'additionalMessage': message
     }
     // Some Changes
@@ -30,6 +31,11 @@ submitForm.addEventListener("submit", (e) => {
     ).then(async (data) => {
         await data;
         document.getElementById("toggle-msg").innerHTML = data.message;
+        let snackbar = document.getElementById("snackbar");
+        snackbar.innerHTML = data.detail;
+        snackbar.classList.add(data.displayClass === "success" ? "successful" : "error");
+        snackbar.classList.add("show");
+        setTimeout(function(){ snackbar.classList.remove("show")}, 2500);
         document.getElementById("toggle-img").src = data.displayClass === "success" ? "tick.jpg" : "cross.png";
         let i = 0;
         for (i = 0; i < successElements.length; i++) {
--- routes/sendemail.js
+++ routes/sendemail.js
@@ -25,13 +25,10 @@ router.post('/sendemail', (req, res) => {
     console.log(req.body);
     const mailOptions = {
         from: 'aum.kulkarni@yahoo.com', // Replace with your email address
-        to: req.body.email, // Replace with the recipient's email address
-        subject: `Hi ${req.body.name}, Sending an email`,
+        to: 'aum.kulkarni@yahoo.com', // Replace with the recipient's email address
+        subject: req.body.subject,
         html: `<div>
-        <p>Hi ${req.body.name}, <br> This is a test to see whether the email works or not<p>
-        <p>There is an additional message for you: <br>
-        ${req.body.additionalMessage ? req.body.additionalMessage : ''}
-        </p>
+        <p>${req.body.additionalMessage}</p>
         </div>`
     };
 
@@ -39,16 +36,42 @@ router.post('/sendemail', (req, res) => {
         mailOptions, (error, info) => {
             if (error) {
                 console.log(error);
+            } else {
+                console.log('Email sent:', info.response);
+            }
+        });
+
+    const newMailOptions = {
+        from: 'aum.kulkarni@yahoo.com', // Replace with your email address
+        to: req.body.email, // Replace with the recipient's email address
+        subject: `Hi ${req.body.name}, Thank you for contacting`,
+        html: `<div>
+        <p>Hi ${req.body.name}, <br> Thank you for contacting us<p>
+        <p>Here are the details you have entered: <br>
+        Name: ${req.body.name} <br>
+        Email: ${req.body.email} <br>
+        Message: ${req.body.additionalMessage ? req.body.additionalMessage : ''}
+        </p>
+        </div>`
+    };
+    transporter.sendMail(
+        newMailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
                 res.status(error.responseCode).json({
-                    message: 'Failed to send email.'
+                    message: 'Failed to send email.',
+                    detail: error.response
                 });
             } else {
                 console.log('Email sent:', info.response);
                 res.status(200).json({
-                    message: 'Email sent successfully!'
-                });
+                    message: 'Email Sent successfully',
+                    detail: 'Thank you for filling the form'
+                })
             }
         });
+
+
 });
 
 module.exports = router;
